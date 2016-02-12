582bbf19432215e2a1d916e0ae1f531b0fc03caf
Mokeev Andrey <andrey@mokeev1995.ru>
2016-02-13 02:13:51 +0300

    project cleanup and fixed versioning

--- Lab1/FreqResult.cs
+++ Lab1/FreqResult.cs
@@ -4,13 +4,13 @@ namespace ConsoleApplication1
 {
 	public class FreqResult
 	{
-		public int Count { get; }
-		public IEnumerable<KeyValuePair<string, int>> TopResults { get; }
-
 		public FreqResult(int count, IEnumerable<KeyValuePair<string, int>> topResults)
 		{
 			Count = count;
 			TopResults = topResults;
 		}
+
+		public int Count { get; }
+		public IEnumerable<KeyValuePair<string, int>> TopResults { get; }
 	}
 }
